59a60dd1c360cb0fb89c56070effd37592d17dd5
Minki-kim95 <rlaalsrl7777@gmail.com>
2018-09-16 00:21:34 +0900

    [EDIT]..

--- 11066.c
+++ 11066.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
+#include <limits>
 
-int m[500][500];
+int m[501][501];
 
 int main(void) {
 
 	int T; //test data
 	int K; //number of page
 	int filesize[500]	//each size of file < 10,000
-	int i, j;
+	int i, j, k, x, l;
 
+	printf("adsf");
 	scanf("%d", &T);
 
-	for (i = 0; i < T; i++) {
+	for (x = 0; x < T; x++) {
+		
 		scanf("%d", &K);
 		for (j = 0; j < K; j++)
 			scanf("%d", &filesize[j]);
 		for (j = 1; j < K; j++)
 			m[j][j] = 0;
-		for
+		for (l = 2; l < K; l++) {
+			for (i = 1; i < K - l + 1; i++) {
+				j = i + l - 1;
+				m[i][j] = INT_MAX;
+				for(k = i; )
+			}
+		}
 
 
 	}
